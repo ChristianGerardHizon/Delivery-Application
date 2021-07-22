191635a5ed94a1ea25840b1401ee917afd967216
Christian Hizon <christiangerardhizon@gmail.com>
2021-07-23 02:46:38 +0800

    added refresher

--- lib/features/tracking/presentation/page/tracking_result_page.dart
+++ lib/features/tracking/presentation/page/tracking_result_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import '../../../../injector.dart';
 import '../managers/tracking/tracking_cubit.dart';
@@ -8,7 +9,9 @@ import '../widgets/result_body.dart';
 
 class TrackingResultPage extends StatelessWidget {
   final String? code;
-  const TrackingResultPage({
+  final _refreshController = RefreshController();
+
+  TrackingResultPage({
     Key? key,
     @PathParam('code') this.code,
   }) : super(key: key);
@@ -37,9 +40,18 @@ class TrackingResultPage extends StatelessWidget {
             return state.when(
               initial: _buildInitialState,
               loading: () => const Center(child: Text('Loading')),
-              loaded: (package, list) => ResultBody(
-                package: package,
-                list: list,
+              loaded: (package, list) => SmartRefresher(
+                header: const MaterialClassicHeader(),
+                controller: _refreshController,
+                onRefresh: () async {
+                  cubit.trackProduct(code!);
+                  await Future.delayed(const Duration(seconds: 1));
+                  _refreshController.refreshCompleted();
+                },
+                child: ResultBody(
+                  package: package,
+                  list: list,
+                ),
               ),
               notfound: () => const Center(child: Text('Not Found')),
             );
